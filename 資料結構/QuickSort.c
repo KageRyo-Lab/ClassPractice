0507517834e47660fd98b77eebc1cb4eb6ea40eb
KageRyo <k6.developer@gmail.com>
2022-12-19 14:42:21 +0800

    增加提示功能

--- 資料結構/QuickSort.c
+++ 資料結構/QuickSort.c
@@ -1,48 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*
+思路： 
+１. 如果左端點大於等於右端點，則退出函數。
+２. 設定 pivot 為左端點的值，並設定 i 為左端點下一位，j 為右端點。
+３. 循環直到 i 大於 j。
+４. 如果 i 小於等於 j 且 data[i] 小於 pivot，則 i 加 1。
+５. 如果 i 小於等於 j 且 data[j] 大於等於 pivot，則 j 減 1。
+６. 如果 i 小於 j，則交換 data[i] 與 data[j] 的值。
+７. 將 data[j] 設為左端點的值，並將 pivot 設為 data[j] 的值。
+８. 對左半部分使用快速排序，對右半部分使用快速排序。
+*/ 
 
 // 定義快速排序函數
 void quick_sort(int *data, int left, int right) {
-	if (left >= right) return;   // 如果左端點索引大於或等於右端點索引，則退出函數
-	int pivot = data[left]; 	 // 將陣列的第一個數字設為支點
-	int i = left + 1, j = right; // 將左端點索引加 1，右端點索引不變
+	if (left >= right) return;		// 如果左端點大於等於右端點，則退出函數
+	int pivot = data[left];			// 設定 pivot 為左端點的值
+	int i = left + 1, j = right; 	// 設定 i 為左端點下一位，j 為右端點
 	while (i <= j) {
-		while (i <= j && data[i] < pivot) i++;	// 如果陣列中第 i 個數字小於支點，則將 i 索引加 1
-		while (i <= j && data[j] >= pivot) j--;	// 如果陣列中第 j 個數字大於等於支點，則將 j 索引減 1 
+		while (i <= j && data[i] < pivot) i++; 	// 如果 i 小於等於 j 且 data[i] 小於 pivot，則 i 加 1 
+		while (i <= j && data[j] >= pivot) j--; // 如果 i 小於等於 j 且 data[j] 大於等於 pivot，則 j 減 1 
 		if (i < j) {
-		    	int temp = data[i];
-		    	data[i] = data[j];
-		    	data[j] = temp;
-		    }
+		    int temp = data[i];
+			data[i] = data[j];
+			data[j] = temp;
+		}
 	}
-	data[left] = data[j];
-	data[j] = pivot;
-	quick_sort(data, left, j - 1);
-	quick_sort(data, j + 1, right);
+	data[left] = data[j]; 			// 將 data[j] 設為左端點的值 
+	data[j] = pivot;				// 將 pivot 設為 data[j] 的值
+	quick_sort(data, left, j - 1);	// 對左半部分使用快速排序
+	quick_sort(data, j + 1, right);	// 對右半部分使用快速排序
 }
 
 // 主程式
 int main() {
-	printf("請輸入要排序的數字個數：\n");
-	int num;
-	scanf("%d", &num);
-	
-	printf("請輸入數字：\n");
-	int *data = (int*)malloc(num * sizeof(int));
-	for (int i = 0; i < num; i++) {
-		printf("請輸入第 %d 個數字：", i + 1);
+	int n;
+	// 提供使用者輸入並檢查是否於限定範圍內 
+	do{
+		printf("請輸入資料筆數 (n<=10): ");
+		scanf("%d", &n);
+		if(n > 10) printf("您輸入的為: %d, 請注意範圍: n<=10\n",n);
+	}while(n > 10);
+	 
+	int originalNumbers[n];
+	int sortedNumbers[n];
+	int isSameOrder;
+	// 提供使用者依序輸入資料 
+	int *data = (int*)malloc(n * sizeof(int));
+	for (int i = 0; i < n; i++) {
+		printf("請輸入第 %d 筆資料: ", i + 1);
 		scanf("%d", &data[i]);
 	}
-	
-	printf("原始的數字：");
-	for (int i = 0; i < num; i++) printf("%d ", data[i]);
+	// 顯示原始輸入的數字順序 
+	printf("\n原始的數字: ");
+	for (int i = 0; i < n; i++) {
+		printf("%d ", data[i]);
+		originalNumbers[i]=data[i];
+	}
 	printf("\n");
-	
-	quick_sort(data, 0, num - 1);
-	printf("排序後的數字：");
-	for (int i = 0; i < num; i++) printf("%d ", data[i]);
+	// 顯示排序後的結果 
+	quick_sort(data, 0, n - 1);
+	printf("\n排序後的數字: ");
+	for (int i = 0; i < n; i++) {
+		printf("%d ", data[i]);
+		sortedNumbers[i]=data[i];
+		if (originalNumbers[i] == sortedNumbers[i]) isSameOrder=1;
+		else isSameOrder=0;
+	}	
 	printf("\n");
+	// 告知使用者原始數字與排序後的數字是否相符(即原輸入順序是否已為排序後結果) 
+	if(isSameOrder==1) printf("原始數字與排序後的數字順序相同");
+	else printf("原始數字與排序後的數字順序不同");
 	
 	free(data);
 	return 0;
 }
+
