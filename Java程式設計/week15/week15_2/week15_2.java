96ed75fce17b37528c400ba74b5cc1ee419d011e
KageRyo <36478298+KageRyo@users.noreply.github.com>
2022-12-21 14:34:20 +0800

    Java : 第十五週

---
+++ Java程式設計/week15/week15_2/week15_2.java
@@ -0,0 +1,24 @@
+package week15_2;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class week15_2 {
+    public static void main(String args[]){
+        byte[] b=new byte[1024]; // 設置位元組緩衝區
+        int n=1;
+        System.out.println("請輸入要讀取的檔案名：例如（d:\\hello.txt）");
+        Scanner scanner = new Scanner(System.in);
+        String str=scanner.nextLine();
+        try{
+            FileInputStream fileInputStream=new FileInputStream(str);
+            while((n= fileInputStream.read(b,0,1024))!=-1){
+                String show=new String(b,0,n);
+                System.out.println(show);
+            }
+        }catch (IOException e){
+            System.out.println("檔案讀取錯誤");
+        }
+    }
+}
