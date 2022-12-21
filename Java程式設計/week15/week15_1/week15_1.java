67a74354a1ad7b845d1c802cd95706a5ac731415
KageRyo <36478298+KageRyo@users.noreply.github.com>
2022-12-21 14:28:55 +0800

    加上註解

--- Java程式設計/week15/week15_1/week15_1.java
+++ Java程式設計/week15/week15_1/week15_1.java
@@ -5,18 +5,22 @@ import java.util.Scanner;
 
 public class week15_1 {
     public static void main(String args[]){
+        // 建立檔案物件
         File file = new File("src/week15_1/record.txt");
+        // 建立讀取器物件
         Scanner reader = null;
+        // 定義價錢變數總和變數
         double price,total=0;
         try{
-            reader = new Scanner(file);
-            reader.useDelimiter("[^0-9.]+");
+            reader = new Scanner(file); // 初始化讀取器，將檔案傳入
+            reader.useDelimiter("[^0-9.]+"); // 設定分隔符為非數字和小數點的任意字元
             while(reader.hasNextDouble()){
                 price = reader.nextDouble();
                 total = total + price;
             }
             System.out.println("總花費："+total+"元");
         }catch (Exception e){
+            // 如果發生錯誤，輸出錯誤訊息
             System.out.println(e.getMessage());
         }
     }
