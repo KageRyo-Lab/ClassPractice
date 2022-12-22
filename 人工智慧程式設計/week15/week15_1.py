69c9626567970d207514d2b8ab4c49d601a7a670
KageRyo <36478298+KageRyo@users.noreply.github.com>
2022-12-22 10:02:23 +0800

    透過 pandas 改良程式碼

--- 人工智慧程式設計/week15/week15_1.py
+++ 人工智慧程式設計/week15/week15_1.py
@@ -4,22 +4,15 @@ s=pd.Series([77,34,78,20,12,35])
 
 print("question1: ")
 # 提取s中索引前三個元素
-s1=s[0:3:1]
+s1=s[:3]
 print(s1)
 
 print("\nquestion2: ")
 # 計算 2s+(s^2)
-for i in range(len(s)):
-    s2=2*s[i]+(s[i]**2)
-    print(s2,end=' ')
+s2=2*s+(s**2)
+print(s2)
 
 print("\n\nquestion3: ")
 # 提取s中的偶數並將它們平均
-s3=0
-count=0
-for i in range(len(s)):
-    if(s[i]%2==0):
-        s3+=s[i]
-        count+=1
-s3=s3/count
+s3=s[s%2==0].mean()
 print(s3)
