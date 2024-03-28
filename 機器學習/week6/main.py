914b279a1f919d356bd3c6a7a54cbc26a17b3aa1
Ryo <k6.developer@gmail.com>
2024-03-28 11:54:38 +0800

    upload: week6 data.

---
+++ 機器學習/week6/main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 28 10:35:03 2024
+
+@author: KageRyo
+"""
+
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv('./Big Data Sample (CSV).csv')   # 相對路徑
+print(data)
+
