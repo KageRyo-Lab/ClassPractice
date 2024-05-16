667adadf1a91b347f37025e251e31cc42d2a7c3c
Ryo <k6.developer@gmail.com>
2024-05-16 11:45:45 +0800

    add plot_decision_boundary func.

--- 機器學習/week13/iris.py
+++ 機器學習/week13/iris.py
@@ -137,3 +137,33 @@ for idx in df_roc.index:
     ax.text(x=df_roc.loc[idx, '1-特異度'], y=df_roc.loc[idx, '敏感度'] - 0.05, s=df_roc.loc[idx, '門檻'].round(1))
 
 plt.show()
+
+# 預測邊界繪製
+def plot_decision_boundary(X_test, y_test, model, debug=False):
+    points=500
+    x1_max, x2_max= X_test.max()
+    x1_min, x2_min= X_test.min()
+    X1,X2= np.meshgrid(np.linspace(x1_min-0.1, x1_max+0.1, points),np.linspace(x2_min-0.1, x2_max+0.1, points))
+    x1_label, x2_label= X_test.columns
+    fig, ax= plt.subplots()
+    X_test.plot(kind='scatter', x=x1_label, y=x2_label, c=y_test, cmap='coolwarm', colorbar= False, figsize=(6,4), s=30, ax=ax)
+    grids=np.array(list(zip(X1.ravel(),X2.ravel())))
+    ax.contourf(X1, X2, model.predict(grids).reshape(X1.shape),alpha=0.3, cmap='coolwarm')
+    if debug:
+        df_debug=X_test.copy()
+        df_debug['y_test']=y_test
+        y_pred=model.predict(X_test)
+        df_debug['y_pred']=y_pred
+        df_debug=df_debug[y_pred != y_test]
+        df_debug.plot(kind='scatter', x=x1_label, y=x2_label, s=50, color='none', edgecolor='y', ax=ax)
+        for i in df_debug.index:
+            ax.text(s=df_debug.loc[i, 'y_test'], x=df_debug.loc[i, x1_label]+0.01, y=df_debug.loc[i, x2_label]-0.05)
+plot_decision_boundary(X_test, y_test, model, debug=True)
+
+# 羅吉斯迴歸預測邊界
+plot_decision_boundary(X_test, y_test, model, debug=True)
+plot_decision_boundary(X_test, y_test, model_pl, debug=True)
+print(df[(df['sepal width (cm)']==2.8) & (df['petal length (cm)']==4.8)])
+
+# 運用模型來預測結果
+model_pl.predict([[3,3]]) 
